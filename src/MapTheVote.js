a3e7fd1acee3d3301a756a4ca79a0652dfb6a6fb
Carl (Subzidion) Hiltbrunner <carlhiltbrunner@hotmail.com>
2019-11-14 00:07:02 -0800

    Add drop down to choose between different maps.

--- src/MapTheVote.js
+++ src/MapTheVote.js
@@ -1,32 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import './MapTheVote.css';
+import React, { useState, useEffect } from "react";
+import { Cascader } from "antd";
+import RenderMap from "./RenderMap";
+import "./MapTheVote.css";
+import 'antd/dist/antd.css';
 
-import RenderMap from './RenderMap';
+var map_source = {"neighborhoods": "https://opendata.arcgis.com/datasets/b76cdd45f7b54f2a96c5e97f2dda3408_2.geojson", 
+                      "sound_transit": "https://transit.land/api/v1/operators/o-c23-soundtransit.geojson",
+                      "counties": "https://opendata.arcgis.com/datasets/12712f465fc44fb58328c6e0255ca27e_11.geojson",
+                      "districts": "https://opendata.arcgis.com/datasets/f14766ecd6274702bd786b214cb916e7_0.geojson"};
+
+var district_colors = {"1": "#440000", "2": "#004400", "3": "#000044", "4": "#444400", "5": "#440044", "6": "#004444", "7": "#444444"};
+var neighborhood_colors = {};
+var county_colors = {};
+
+const map_picker_options = [
+  {
+    value: "neighborhoods",
+    label: "Neighborhoods"
+  },
+  {
+    value: "districts",
+    label: "Districts"
+  },
+  {
+    value: "counties",
+    label: "Counties"
+  },
+  {
+    value: "sound_transit",
+    label: "Sound Transit"
+  }
+];
 
 function MapTheVote() {
   const [mapGeometry, setMapGeometry] = useState();
+  const [map, setMap] = useState(["neighborhoods"]);
 
-  function fetchGeometry() {
-    fetch("https://opendata.arcgis.com/datasets/f14766ecd6274702bd786b214cb916e7_0.geojson")
-    .then(response => response.json())
-    .then(data => setMapGeometry(data));
-  }
+  useEffect(() => {
+    function fetchGeometry() {
+      fetch(map_source[map[0]])
+      .then(response => response.json())
+      .then(data => setMapGeometry(data));
+    }
 
-  useEffect(() => fetchGeometry(), []);
+    fetchGeometry();
+  }, [map]);
 
-  if(mapGeometry) {
-    return (
-      <div className="container">
-        <RenderMap geometry={mapGeometry} />
-      </div>
-    );
+  let render_map;
+  if(mapGeometry && map[0] === "districts") {
+    render_map = <RenderMap map={map[0]} geometry={mapGeometry} label="District" identifier="C_DISTRICT" colors={district_colors} />
+  } else if(mapGeometry && map[0] === "neighborhoods") {
+    render_map = <RenderMap map={map[0]} geometry={mapGeometry} label="Neighborhood" identifier="S_HOOD" colors={neighborhood_colors} />
+  } else if(mapGeometry && map[0] === "sound_transit") {
+    render_map = <RenderMap map={map[0]} geometry={mapGeometry} label="Sound Transit" identifier="S_HOOD" />
+  } else if(mapGeometry && map[0] === "counties") {
+    render_map = <RenderMap map={map[0]} geometry={mapGeometry} label="" identifier="JURISDICT_NM" colors={county_colors} />
   } else {
-    return (
-      <div className="container">
-        Loading...
-      </div>
-    );
+    render_map = "Loading..."
   }
+
+  return (
+    <div className="container">
+      <header>Map The Vote</header>
+      <Cascader options={map_picker_options} onChange={setMap} defaultValue={["neighborhoods"]} placeholder="Select Map" />
+      <div>
+        { render_map }
+      </div>
+    </div>
+  );
 }
 
 export default MapTheVote;
